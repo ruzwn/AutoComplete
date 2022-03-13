0857ad2539110df7f58b5f0e814f5836cf55d52a
ruzwn <ruzwn@users.noreply.github.com>
2022-03-13 18:54:40 +0300

    Third version

--- AutoComplete.cs
+++ AutoComplete.cs
@@ -1,55 +1,122 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace AutoComplete
 {
-
     public struct FullName
     {
         public string Name;
         public string Surname;
         public string Patronymic;
 
-        public override string ToString()
+        public override string ToString() =>
+            string.Join(" ", Surname?.Trim(), Name?.Trim(), Patronymic?.Trim()).Trim();
+        
+        public override bool Equals(object? obj)
         {
-            return string.Join(" ", Surname?.Trim(), Name?.Trim(), Patronymic?.Trim()).Trim();
+            if (obj == null)
+                throw new ArgumentNullException();
+            
+            var fullName = (FullName) obj;
+
+            return
+                Name == fullName.Name &&
+                Surname == fullName.Surname &&
+                Patronymic == fullName.Patronymic;
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = 19;
+
+            if (Surname != null)
+                hash = 23 * hash + Surname.GetHashCode();
+            if (Name != null)
+                hash = 23 * hash + Name.GetHashCode();
+            if (Patronymic != null)
+                hash = 23 * hash + Patronymic.GetHashCode();
+
+            return hash;
+        }
+    }
+    
+    public class TrieNode
+    {
+        FullName? _data;
+        readonly Dictionary<char, TrieNode> _children = new();
+
+        public void Add(string path, FullName data)
+        {
+            Add(0, path, data);
+        }
+
+        void Add(int index, string path, FullName data)
+        {
+            if (index == path.Length)
+            {
+                _data = data;
+                return;
+            }
+
+            var key = path[index];
+            if (!_children.ContainsKey(key))
+                _children.Add(key, new TrieNode());
+            
+            _children[key].Add(index + 1, path, data);
+        }
+
+        public void GetByPrefix(string prefix, List<string> result) =>
+            GetByPrefix(0, prefix, result);
+
+        void GetByPrefix(int index, string prefix, List<string> result)
+        {
+            if (_data.HasValue)
+                result.Add(_data.Value.ToString());
+            
+            if (index == prefix.Length)
+            {
+                foreach (var childValue in _children.Values)
+                    childValue.GetByPrefix(index, prefix, result);
+            }
+            else
+            {
+                var key = prefix[index];
+                if (_children.ContainsKey(key))
+                    _children[key].GetByPrefix(index + 1, prefix, result);
+            }
         }
     }
 
     public class AutoCompleter
     {
-        List<string> _fullNames;
-        
+        readonly TrieNode _trie = new();
+
         public void AddToSearch(List<FullName> fullNames)
         {
-            //Добавьте новый элемент чтобы он начал участвовать в поиске по фио
             if (fullNames == null)
                 throw new ArgumentNullException();
-            
-            _fullNames = new List<string>();
-            
+
             foreach (var fullName in fullNames)
             {
                 var fullNameStr = fullName.ToString();
                 if (fullNameStr == string.Empty)
                     throw new ArgumentException();
-                _fullNames.Add(fullNameStr);
+                _trie.Add(fullNameStr, fullName);
             }
         }
 
         public List<string> Search(string prefix)
         {
-            //Реализуйте алгоритм поиска по префиксу фио, как составить фио смотри условия задачи.
             if (string.IsNullOrEmpty(prefix) || string.IsNullOrWhiteSpace(prefix))
                 throw new ArgumentException();
-            
-            if (prefix.Length > 100) // нужно ли считать пробелы ?
+
+            if (prefix.Length > 100)
                 throw new ArgumentOutOfRangeException();
             
-            // нужно ли как-то преобразовывать prefix (убирать пробелы в начале, конце и лишние посередине)?
+            var result = new List<string>();
+            _trie.GetByPrefix(prefix, result);
 
-            return _fullNames.Where(fullName => fullName.StartsWith(prefix)).ToList();
+            return result;
         }
     }
 }
